c5695dbb0f795e2d508efc38c740c5f138f37058
jdshap <jdshap@comcast.net>
2019-03-16 20:30:36 -0500

    Add DELETE method

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const db = require('diskdb');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 
+// Connect to diskdb database "responses"
 db.connect('db', ['responses']);
 
+/*
+ *  GET method
+ *  @query-string secondsAgo - filter results by timestamp recency
+ */
 app.get('/testresults', (req, res) => {
     let data = db.responses.find();
+
+    //Filter by secondsAgo if defined
     if(req.query.secondsAgo) {
         const temp = JSON.parse(JSON.stringify(data));
         data.length = 0;
@@ -15,12 +23,19 @@ app.get('/testresults', (req, res) => {
             if(Date.now() - o.timestamp <= parseInt(req.query.secondsAgo) * 1000) data.push(o);
         }, []);
     }
-    console.log(data);
+    
     res.send(data);
 });
 
+/*
+ *  GET method
+ *  @route-parameter package - filters by specified package value
+ *  @query-string secondsAgo - filter results by timestamp recency
+ */
 app.get('/testresults/:package/', (req, res) => {
     let data = db.responses.find({package: req.params.package});
+
+    //Filter by secondsAgo if defined
     if(req.query.secondsAgo) {
         const temp = JSON.parse(JSON.stringify(data));
         data.length = 0;
@@ -28,11 +43,20 @@ app.get('/testresults/:package/', (req, res) => {
             if(Date.now() - o.timestamp <= parseInt(req.query.secondsAgo) * 1000) data.push(o);
         }, []);
     }
+
     res.send(data);
 });
 
+/*
+ *  GET method
+ *  @route-parameter package - filters by specified package value
+ *  @route-parameter id - filters by specified id value (Not database _id hash)
+ *  @query-string secondsAgo - filter results by timestamp recency
+ */
 app.get('/testresults/:package/:id', (req, res) => {
     let data = db.responses.find({package: req.params.package, id: parseInt(req.params.id)});
+
+    //Filter by secondsAgo if defined
     if(req.query.secondsAgo) {
         const temp = JSON.parse(JSON.stringify(data));
         data.length = 0;
@@ -40,9 +64,13 @@ app.get('/testresults/:package/:id', (req, res) => {
             if(Date.now() - o.timestamp <= parseInt(req.query.secondsAgo) * 1000) data.push(o);
         }, []);
     }
+
     res.send(data);
 });
 
+/*
+ *  POST method
+ */
 app.post('/testresults', (req, res) => {
     let result = {
         package: req.body.package,
@@ -54,6 +82,18 @@ app.post('/testresults', (req, res) => {
     };
     db.responses.save(result);
     res.send(result);
+});
+
+/*
+ *  DELETE method
+ *  Backs up database, then clears it.
+ */
+app.delete('/testresults', (req, res) => {
+    const json = JSON.stringify(db.responses.find());
+    fs.writeFile(`db/backup-${Date.now()}`, json, 'utf8', (err) => { if (err) throw err });
+    db.responses.remove();
+    db.connect('db', ['responses']);
+    res.send('DELETE complete');
 });
 
 const port = process.env.PORT || 3000;
